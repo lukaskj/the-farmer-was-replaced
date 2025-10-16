76294d0a78be032e311e9e570e8b445bc5e3d46e
Lukas K Jacomin <lukasjacomin@gmail.com>
2025-10-16 02:47:50 -0300

    feat: implement drone planting functionality for pumpkin crops and update save data

--- Saves/Save0/plant_pumpkin.py
+++ Saves/Save0/plant_pumpkin.py
@@ -1,16 +1,37 @@
 import utils
+import drones
 # from globals import w, h
 
 totalGrown = 0
 addFertilizer = False
 deadPlants = []
 
+def _plantPunpkins(seed, startX, startY, width, height):
+  # quick_print(seed, startX, startY, width, height)
+  pass
+
+def __spawnDrone():
+  def run(gridData):
+    seed = Entities.Pumpkin
+    startX, startY, width, height = gridData
+    def __():
+      for _ in range(width * height):
+        _plantPunpkins(seed, startX, startY, width, height)
+        utils.plantSeed(seed)
+        nextX, nextY = utils.getNextSubgridPos(width, height, startX, startY)
+        utils.moveTo(nextX, nextY)
+      
+      # utils.sleep(10)
+    return __
+  return run
+
+
 def _plant_pumpkin_field(maxW, maxH):
+  seed = Entities.Pumpkin
   for row in range(maxH):
     for col in range(maxW):
-      seed = Entities.Pumpkin
       utils.moveTo(col, row)
-      if get_entity_type() == Entities.Pumpkin:
+      if get_entity_type() == seed:
         continue
       plant_and_fertilize(col, row)
       # if (col % 2 == 0 and row % 2 != 0) or (col % 2 != 0 and row % 2 == 0):
@@ -31,16 +52,16 @@ def _calculate_dead_plants(maxW, maxH):
         if get_water() < 0.11:
           use_item(Items.Water)
   # utils.sort_coordinates(deadPlants)
-  quick_print("Total grown: " + str(totalGrown) + "/" + str(maxW * maxH))
-  quick_print("Dead plants at: " + str(deadPlants))
+  # quick_print("Total grown: " + str(totalGrown) + "/" + str(maxW * maxH))
+  # quick_print("Dead plants at: " + str(deadPlants))
 
 def _replant_dead():
   global deadPlants
   global totalGrown
   
   while len(deadPlants) > 0:
-    if len(deadPlants) > 1:
-      quick_print("Replanting dead plants, left: " + str(deadPlants))
+    # if len(deadPlants) > 1:
+    #   quick_print("Replanting dead plants, left: " + str(deadPlants))
     for (col, row) in deadPlants:
       utils.moveTo(col, row)
       if can_harvest() and get_entity_type() == Entities.Pumpkin:
@@ -71,10 +92,12 @@ def start(maxW, maxH):
   while not harvested:
     # addFertilizer = num_items(Items.Weird_Substance) < 100 and num_items(Items.Fertilizer) > 100
     quick_print("Add fertilizer: " + str(addFertilizer))
-    _plant_pumpkin_field(maxW, maxH)
+    # _plant_pumpkin_field(maxW, maxH)
+    drones.droneGrid(maxW, maxH, __spawnDrone(), None)
+    drones.waitForAllDronesToFinish()
     _calculate_dead_plants(maxW, maxH)
     _replant_dead()
-    if can_harvest() and totalGrown == maxW * maxH:
+    if can_harvest():
       # do_a_flip()
       before = num_items(Items.Pumpkin)
       totalGrown = 0
@@ -86,8 +109,10 @@ def start(maxW, maxH):
 
 
 if __name__ == "__main__":
-  maxW2 = 6
-  maxH2 = 6
-  start(maxW2, maxH2)
+  maxW2 = 10
+  maxH2 = 10
+  runs = 10
+  for _ in range(runs):
+    start(maxW2, maxH2)
   # clear()
   # harvest()
